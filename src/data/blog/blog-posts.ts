d07cf9e6d15245c9aab58961980a6d687b6bd787
Alex Caras <alexcaras1@hotmail.com>
2026-05-11 22:05:00 -0300

    chore(blog): remove local blog content and legacy blog images

--- src/data/blog/blog-posts.ts
+++
@@ -1,9 +0,0 @@
-import checklistNutricionalMounjaroOzempic from './checklist-nutricional-mounjaro-ozempic'
-import dietaLowCarbGuiaCompletoSeguro from './dieta-low-carb-guia-completo-seguro'
-
-const blogPosts = [
-  dietaLowCarbGuiaCompletoSeguro,
-  checklistNutricionalMounjaroOzempic,
-]
-
-export default blogPosts
